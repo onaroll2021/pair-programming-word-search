5797d36e3010048fe915beca2c43388593e1fdb7
onaroll2021 <lukeli.onaroll@gmail.com>
2022-09-29 05:49:07 +0000

    complete version

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -9,15 +9,15 @@ describe("#wordSearch()", function() {
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['H', 'R', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'A', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'K', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
 
-    assert.isFalse(result);
+    assert.isTrue(result);
   });
 
   it("should return true if the word is present", function() {
@@ -36,3 +36,75 @@ describe("#wordSearch()", function() {
     assert.isTrue(result);
   });
 });
+
+// const chai = require('chai');
+// const assert = chai.assert;
+
+// const wordSearch = require('../wordsearch.js')
+
+// describe("#wordSearch()", function() {
+//   it("should return false if the word is not present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'FRANK')
+
+//     assert.isFalse(result);
+//   });
+
+//   it("should return true if the word is present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'SEINFELD')
+
+//     assert.isTrue(result);
+//   });
+
+//   it("should return true if the word is present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'ASY')
+
+//     assert.isTrue(result);
+//   });
+
+//   it("should return true if the word is present", function() {
+//     const result = wordSearch([
+//       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//     ], 'VEAP')
+
+//     assert.isFalse(result);
+//   });
+// });
+
--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,52 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let letterArrs = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        let letterName = '';
+        for (let u = 0; u < letters.length; u++) {
+            letterName += letters[u][i];
+        }
+        letterArrs.push(letterName);
+    }
+    for (const r of letterArrs) {
+        if (r.includes(word)) return true
+    }
+    return false;
 }
 
+
 module.exports = wordSearch
+
+// const wordSearch = (letters, word) => { 
+//     const horizontalJoin = letters.map(ls => ls.join(''));
+//     //console.log(horizontalJoin);
+//     for (l of horizontalJoin) {
+//         if (l.includes(word)) { return true } 
+//     }
+    
+//     //let verticleStr = "";
+
+//     for(let i = 0; i < letters[0].length; i++) {
+//         let verticleStr = "";
+//         for(let j = 0; j < letters.length; j++) {
+//             verticleStr += letters[j][i];
+//         }
+//         //console.log(verticleStr);
+//         if(verticleStr.includes(word)){
+//             //console.log(VerticleStr);
+//             return true;
+//         }
+//     }
+
+
+//     //console.log(verticleStr);
+
+
+//     return false;
+
+
+
+// }
+
+// module.exports = wordSearch;
